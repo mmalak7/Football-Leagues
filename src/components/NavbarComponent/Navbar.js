101e40551193c1de67b720d54de7e2040b6e784c
mmalak7 <marcinmalak7@gmail.com>
2020-08-06 17:45:24 +0200

    add switch button navbar for dark/light mode

--- src/components/NavbarComponent/Navbar.js
+++ src/components/NavbarComponent/Navbar.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 import "./Navbar.scss";
 
 export default function Navbar() {
+  const [state, setState] = useState({ checked: true });
+
+  const handleChange = (event) => {
+    setState({ [event.target.name]: event.target.checked });
+  };
+
   return (
     <div className="navbar-wrapper">
       <div className="navbar navbar-items">
         <div className="navbar navbar-item logo">Football Leagues</div>
         <div className="navbar navbar-item mode">
-          {/* svg here albo component ze zmiana theme */}
-          <p>Dark Mode</p>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={state.checked}
+                  onChange={handleChange}
+                  name="checked"
+                />
+              }
+              label="Dark Mode"
+            />
+          </FormGroup>
         </div>
       </div>
     </div>
